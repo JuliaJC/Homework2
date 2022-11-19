2fdaf77fe4c8f96de1ffa3d39c389ffc7be9c569
Julia <julso@LAPTOP-J5CQF40T.(none)>
2022-11-19 13:14:22 +0300

    Zadacha10  added

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number<1000 && number>99)
+{
+    int number2 = number / 10;
+    int secondDigit = number2 % 10;
+    Console.WriteLine(secondDigit);
+}
+
+else
+{
+    Console.WriteLine("Введенное число не является трехзначным.");
+}
